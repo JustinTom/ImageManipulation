ed2b004b64f370599363970119810189650d1462
JustinTom <cyclostofu@hotmail.com>
2015-09-25 21:30:01 -0700

    Added more specific comments to the rotate function as well as pre-requisites for the python program (pillow library)

--- ImageManipulation.py
+++ ImageManipulation.py
@@ -6,6 +6,8 @@ def rotate(imgObj):
 	#If the degree isn't a 90 degree variable, it will fill a black background to accomodate the extra space
 	rotationDegree = raw_input("How much would you like to rotate the image (clock-wise)? \n")
 	#The true rotate function.
+	#Finds the center of the image based on image size (height and width) and rearranges the pixels based on the rotation degree
+	#using some triggonometric calculations with sin and cos math functions. 
 	manipulatedImage = imgObj.rotate(float(rotationDegree), expand=1)
 	#Show the rotated, manipulated image 
 	manipulatedImage.show()
--- Readme.txt
+++ Readme.txt
@@ -4,6 +4,9 @@ Languages Used:
 - Rotation (Python)
 - Collage (Java)
 
+Pre-requisites:
+- For the Python program, it requires the Python library Pillow to properly run.
+
 Run instructions:
 	Rotation: python ImageManipulation.py
 
